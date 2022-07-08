0aa30764d7732f494d724c4a6b1972087ea37ad2
Thina <Thina@zipcodersmbp42.mynetworksettings.com>
2022-07-08 16:31:03 -0400

    updated fizz_buzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,9 +1,9 @@
 for number in range(1,101):
-    if number%3 == 0:
+    if number%3 == 0 and number%5 ==0:
+        print("FizzBuzz")
+    elif number%3 == 0:
         print("Fizz")
     elif number%5 ==0:
         print("Buzz")
-    elif number%3 == 0 and number%5 ==0:
-        print("FizzBuzz")
     else:
         print(f"{number} new line")
